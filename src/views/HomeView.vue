ada915b1006c67d8dedbd0b7762198fc0e90826a
Mari <testirovsik0005@mail.ru>
2024-12-13 09:45:11 +0300

    сделала адаптив для блока с голубем

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,13 +14,16 @@
 </template>
 <style>
 .bird-lay{
-    display: flex;
-    align-items: center;
-    justify-content: center;
     max-width: 1110px;
     margin: 0 auto;
     padding: 0 15px;
     gap: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.Title-main {
+    color: #598D66;
 }
 .golub-pic {
     width: 540px;
@@ -41,9 +44,6 @@
     color: #2C2D35;
     margin-bottom: 20px;
 }
-.Title-main {
-    color: #598D66;
-}
 .block2 {
     font-size: 18px;
     font-weight: 500;
@@ -52,15 +52,96 @@
     margin-bottom: 60px;
 }
 .product.button {
+    width: 225px;
+    height: 65px;
     font-size: 20px;
     font-weight: 500;
     line-height: 23.48px;
     color: #598D66;
     border: 1px solid #598D66;
     background-color: transparent;
-    width: 225px;
-    height: 65px;
     cursor: pointer;
+}
+@media screen and (width <= 577px) {
+    .block1 {
+        line-height: 36px;
+        margin-bottom: 10px;
+        font-size: 36px;
+        font-weight: 500;
+    }
+    .golub-pic {
+        height: 239px;
+        width: 195px;
+    }
+    .bird-lay {
+        margin-top: 50px;
+        padding: 0 33px;
+    }
+    .block2 {
+        line-height: 24px;
+        margin-bottom: 11px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+    .product.button  {
+        width: 178px;
+        height: 50px;
+        font-family: Raleway;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 21.13px;
+    }
+}
+
+@media screen and (width <= 769px) {
+    .golub-pic {
+        height: 380px;
+        width: 330px;
+    }
+    .bird-lay {
+        padding: 0 39px;
+    }
+    .block2 {
+        margin-bottom: 40px;
+    }
+    .block1 {
+        font-size: 40px;
+        font-weight: 500;
+        line-height: 40px;
+    }
+    .product.button {
+        height: 60px;
+        width: 210px;
+    }
+}
+@media screen and (width <= 376px) {
+    .golub-pic {
+        display: none;
+    }
+    .bird-lay {
+        margin-top: 60px;
+        padding: 0 15px;
+    }
+    .block1 {
+        line-height: 30px;
+        margin-bottom: 20px;
+        font-size: 30px;
+        font-weight: 500;
+    }
+    .block2 {
+        line-height: 21px;
+        margin-bottom: 40px;
+        font-size: 14px;
+        font-weight: 500;
+    }
+    .product.button {
+        height: 50px;
+        width: 154px;
+    }
+}
+@font-face {
+    font-family: Raleway;
+    src: url("../assets/fonts/Raleway-VariableFont_wght.ttf");
 }
 </style>
 
